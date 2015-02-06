006032b536e90fc266bf89fbd8e026ab063f585e
Casey Foster <c@sey.me>
2015-02-05 21:03:22 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-# cogs-transformer-example
+# cogs-transformer-directives
 
-An example transformer for [Cogs].
+A directives transformer for [Cogs].
 
-[![Build Status]](http://travis-ci.org/caseywebdev/cogs-transformer-example)
+[![Build Status]](http://travis-ci.org/caseywebdev/cogs-transformer-directives)
+
+Extract `require` and `link` directives from a file's initial comments.
+
+In the following example, the file itself will be proceeded by every file in the
+templates directory.
+
+```js
+//= requireself
+//= require ./templates/**/*
+
+var foo = 'bar';
+```
+
+In this example, the file will be prepended with `normalize.css`.
+
+```css
+/*
+= require bower_components/normalize/normalize.css
+*/
+
+.foo {
+  color: blue;
+}
+```
 
 [Cogs]: https://github.com/caseywebdev/cogs
-[Build Status]: https://secure.travis-ci.org/caseywebdev/cogs-transformer-example.png
+[Build Status]: https://secure.travis-ci.org/caseywebdev/cogs-transformer-directives.png
--- test/test.js
+++ test/test.js
@@ -1,17 +1,57 @@
 var config = require('cogs/src/config');
+var crypto = require('crypto');
 var expect = require('chai').expect;
 var fs = require('fs');
 var getBuild = require('cogs/src/get-build');
+var glob = require('glob');
 var path = require('path');
 
 var beforeEach = global.beforeEach;
 var describe = global.describe;
 var it = global.it;
 
+var getHash = function (buffer) {
+  var hash = crypto.createHash('md5');
+  hash.end(buffer);
+  return hash.read().toString('hex');
+};
+
+var getFileHash = function (filePath) {
+  return getHash(fs.readFileSync(filePath));
+};
+
+var getGlobHash = function (pattern) {
+  return getHash(JSON.stringify(glob.sync(pattern)));
+};
+
 var FIXTURES = {
   'test/config.json': {
-    'test/input.txt': 'test/output.txt',
-    'test/error.txt': Error
+    'test/foo.txt': {
+      path: 'test/foo.txt',
+      buffer: fs.readFileSync('test/output.txt'),
+      hash: getFileHash('test/output.txt'),
+      requires: [{
+        path: 'test/foo.txt',
+        hash: getFileHash('test/foo.txt')
+      }, {
+        path: 'test/bar.txt',
+        hash: getFileHash('test/bar.txt')
+      }, {
+        path: 'test/baz.txt',
+        hash: getFileHash('test/baz.txt')
+      }],
+      links: [{
+        path: 'test/buz.txt',
+        hash: getFileHash('test/buz.txt')
+      }],
+      globs: [{
+        path: 'test/+(bar|baz).txt',
+        hash: getGlobHash('test/+(bar|baz).txt')
+      }]
+    },
+    'test/error.txt': Error,
+    'test/error2.txt': Error,
+    'test/error3.txt': Error
   }
 };
 
@@ -24,19 +64,15 @@ Object.keys(FIXTURES).forEach(function (configPath) {
     });
 
     Object.keys(builds).forEach(function (inputPath) {
-      var outputPath = builds[inputPath];
+      var expected = builds[inputPath];
 
       describe(inputPath, function () {
-        var expectsError = outputPath === Error;
+        var expectsError = expected === Error;
 
         it(expectsError ? 'fails' : 'succeeds', function (done) {
           getBuild(inputPath, function (er, build) {
             if (expectsError) expect(er).to.be.an.instanceOf(Error);
-            else {
-              if (er) return done(er);
-              var output = fs.readFileSync(outputPath);
-              expect(build.buffer).to.deep.equal(output);
-            }
+            else expect(build).to.deep.equal(expected);
             done();
           });
         });
